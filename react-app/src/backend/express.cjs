e8dda49737b932cee2d6447d5a0b58b398089324
botondterenyi <botiterenyi@gmail.com>
2025-09-22 08:46:43 +0200

    GET mysql data

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,15 +1,42 @@
 const express = require('express')
+const mysql = require('mysql')
 const cors = require('cors')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // TODO - credential
+    database: 'test'
+})
+
+
+
+app.get('/mueslis', (req, res) => {
+    //connection.connect(err => console.warn('connection error', err))
+    //console.log('connection', connection)
+
+    connection.query('SELECT id, name, price FROM mueslis', (error, result, fields) => {
+        if (error) console.warn(error)
+        if (result) {
+            console.log(result)
+            console.log(fields)
+
+            res.status(200).json({error, result, fields})
+        } else {
+            res.status(403).json({error})
+        }
+    })
+})
+
 app.get((err, req, res) => {
     if (err) {
         res.status(404).send('<h1>404 Not Found</h1>')
     } else {
-        res.status(200)
+        res.sendStatus(200)
     }
 })
 
